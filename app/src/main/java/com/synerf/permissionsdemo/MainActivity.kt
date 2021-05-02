3847dca8505bd1232a0223bc14c978a028bf8011
Ashish Pimpre <ashishpimpre.connect@gmail.com>
2021-05-02 22:44:53 +0530

    Initial commit

---
+++ app/src/main/java/com/synerf/permissionsdemo/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.synerf.permissionsdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.synerf.permissionsdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCameraPermission.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You already have the permission for camera and gps", Toast.LENGTH_LONG).show()
+            } else {
+                // Request permission
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION),
+                        CAMERA_AND_FINE_LOCATION_PERMISSION_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted for camera and gps", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Oops, you denied the camera and gps permission", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val FINE_LOCATION_PERMISSION_CODE = 2
+        private const val CAMERA_AND_FINE_LOCATION_PERMISSION_CODE = 12
+
+    }
+}
